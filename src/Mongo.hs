af3b3ca52a51b29d1b36170d1198db671fb066f7
Thomas M. DuBuisson <tommd@musedev.io>
2020-02-05 15:59:15 -0800

    Yesod cookbook section on mongo + persistent

---
+++ src/Mongo.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
+import Database.Persist (insert, (==.), selectList, Entity(..))
+import Database.Persist.TH
+import Database.Persist.MongoDB (runMongoDBPool, withMongoDBConn, master, Action)
+import Control.Monad.Cont (liftIO)
+import MongoImport
+import Database.MongoDB.Connection (PortID(PortNumber))
+import Language.Haskell.TH (Type(..))
+
+share
+    [mkPersist mongoSettings]
+    [persistLowerCase|
+User
+    name String
+    age Int Maybe
+    deriving Show
+Blogpost
+    title String
+    uid UserId
+    UniqueUser uid
+    deriving Show
+|]
+
+main :: IO ()
+main =
+    withMongoDBConn
+        "myDatabaseName"
+        "localhost"
+        (PortNumber 27017)
+        Nothing
+        2000
+        (runMongoDBPool
+             master
+             (do user <- insert (User "John Doe" $ Just 35)
+                 liftIO $ print user
+                 vals <- selectList [ UserName ==. "John Doe" ] [] :: Action IO [Entity User]
+                 liftIO $ print (map entityVal vals)
+                 return ()))
