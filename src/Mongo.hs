f8f1dfff7fa92c62d80d7fc27fba44e7bf913352
Thomas M. DuBuisson <tommd@musedev.io>
2020-02-06 14:53:25 -0800

    an example of an index in mongo with persistent

--- src/Mongo.hs
+++ src/Mongo.hs
@@ -14,7 +14,9 @@
 import Database.Persist (insert, (==.), selectList, Entity(..), PersistField(..), PersistValue(PersistByteString))
 import Database.Persist.Sql (PersistFieldSql(..))
 import Database.Persist.TH
-import Database.Persist.MongoDB (runMongoDBPool, withMongoDBConn, master, Action)
+import Database.Persist.MongoDB (runMongoDBPool, withMongoDBConn, master, Action
+                                , (=:))
+import Database.MongoDB.Admin (createIndex,index)
 import Control.Monad.Cont (liftIO)
 import MongoImport
 import Database.MongoDB.Connection (PortID(PortNumber))
@@ -48,4 +50,8 @@ main =
                  liftIO $ print user
                  vals <- selectList [ UserName ==. "John Doe" ] [] :: Action IO [Entity User]
                  liftIO $ print (map entityVal vals)
+                 createIndex $ index "Blogpost" [ "title" =: (1 :: Int) {- accending -}
+                                                , "uid"   =: (negate 1 :: Int) {- decending -} ]
+                 bps <- selectList [ BlogpostTitle ==. "D.N.E." ] [] :: Action IO [Entity Blogpost]
+                 liftIO (print bps)
                  return ()))
